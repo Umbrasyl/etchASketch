50a98d1373fb05e7f271a0224b23668440ddff32
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-11-16 14:33:58 +0300

    Add brush, rainbow, eraser, clear and color picker

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 "use strict";
 
 function createDivs(num) {
-    const divWidth = Math.floor(canvasWidth / num); // Adjust div width so exactly num divs fit in a row
+    const divWidth = Math.round((canvasWidth / num) * 100) / 100; // Adjust div width so exactly num divs fit in a row
     for(let i = 0; i < num**2; i++) { // Num is squared so we have a square canvas width same numbers of rows and columns
         const createdDiv = document.createElement("div");
         createdDiv.style.width = createdDiv.style.height = `${divWidth}px`;
         createdDiv.addEventListener("mouseover", mouseOver);
-        createdDiv.addEventListener("mouseout", mouseOut);
         containerDiv.appendChild(createdDiv);
     }
 }
 
 function mouseOver() {
-    this.classList.add("hover");
-}
-
-function mouseOut() {
-    this.classList.remove("hover");
+    if (mousedown) {
+        if (rainbow) {
+            this.style.backgroundColor = getRandomColor();
+        } else {
+            this.style.backgroundColor = penColor;
+        }
+    }
 }
 
 function createNewCanvas() {
@@ -31,8 +32,57 @@ function createNewCanvas() {
     }
 }
 
+function clearCanvas() {
+    containerDiv.childNodes.forEach((childDiv) => {
+        childDiv.style.backgroundColor = "white";
+    });
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 const containerDiv = document.querySelector(".container");
 const canvasWidth = +(containerDiv.clientWidth);
-const canvasButton = document.querySelector("button");
+
+const colorPicker = document.querySelector("input");
+const brushButton = document.querySelector(".brush");
+const rainbowButton = document.querySelector(".rainbow");
+const eraserButton = document.querySelector(".eraser");
+const clearButton = document.querySelector(".clear");
+const canvasButton = document.querySelector(".new-grid");
+
+
+let penColor = colorPicker.value;
+let rainbow = false;
+
+let mousedown = false;
+containerDiv.addEventListener("mousedown", () => {
+    mousedown = true;
+});
+containerDiv.addEventListener("mouseup", () => {
+    mousedown = false;
+});
+
+colorPicker.addEventListener("input", () => {
+    penColor = colorPicker.value;
+});
+brushButton.addEventListener("click", () => {
+    rainbow = false;
+    penColor = colorPicker.value;
+});
+rainbowButton.addEventListener("click", () => {
+    rainbow = true;
+});
+eraserButton.addEventListener("click", () => {
+    penColor = "white";
+});
+clearButton.addEventListener("click", clearCanvas);
 canvasButton.addEventListener("click", createNewCanvas);
-createDivs(10);
+
+createDivs(20);
